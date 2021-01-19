8f43a3f4084b1b4c6a0283c299531325242322c2
Smagin-Web <smagin.nikolay@yandex.ru>
2021-01-19 19:38:24 +0300

    +catalog-component (взял каталог из компонента header и переместил в отдельный компонент catalog.component..)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SidebarModule } from 'ng-sidebar';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material.module';
 
+import { HeaderComponent } from './header/header.component';
+import { CatalogComponent } from './catalog/catalog.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
